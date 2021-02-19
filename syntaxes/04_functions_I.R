f5acd1db4624de5000af81f6912932e19add4434
Benjamin Becker <benj.becker@web.de>
2021-02-19 16:51:39 +0100

    conceptual framework functions

--- syntaxes/04_functions_I.R
+++ syntaxes/04_functions_I.R
@@ -9,8 +9,42 @@
 #===========================================================
 #...........................
 
+# Why?
+#...........................
+# Readability (shorter, easier understanding, removes distractions, like references in a paper)
+# two of your worst enemies: past self and future self!
+mean(mtcars$mpg)
+# vs.
+sum(mtcars$mpg)/dim(mtcars)[1]
+
+summary(mtcars$mpg)
+# vs.
+round(c("Min." = min(mtcars$mpg),
+  "1st Qu." = as.numeric(quantile(mtcars$mpg)[2]),
+  "Median" = median(mtcars$mpg),
+  "Mean" = mean(mtcars$mpg),
+  "3rd Qu." = as.numeric(quantile(mtcars$mpg)[4]),
+  "Max." = max(mtcars$mpg)), 2)
+
+
+# Transferability (other use cases, other projects, other persons)
+
+
+
+#...........................
+
 # Basics
 #...........................
+# Definition of a function:
+countNA <- function(x) {    # Name, Arguments
+  out <- sum(is.na(x))      # Body
+  out                       # Output
+}
+b <- c(NA, 1, 4, NA, 3)
+countNA(b)
+
+# Inspecting the function ('source code')
+countNA
 
 # functions are objects too!
 mean(1:10)
@@ -20,15 +54,36 @@ my_own_mean_fun(1:10)
 # existing functions
 ncol
 dim # Primitive functions
+lavaan::sem # from packages
+haven:::add_text
+mean # S3 methods
+lme4::getL # S4 methods
+# anonymous functions
 
 # function components (formals, body, environment)
 add_things <- function(x) {
   x + 10
 }
 add_things(2)
+formals(add_things)
+body(add_things)
+environment(add_things)
+library(lavaan)
+environment(sem)
 
 # help pages
 ?mean
+
+
+#...........................
+
+# Formals/Arguments
+#...........................
+### Usual arguments (R as a tool for data science)
+# a) Data input (vector, data.frame etc.)
+# b) Options
+?mean
+?lavaan::sem
 
 # default arguments
 add_things_def <- function(x = 1) {
@@ -36,13 +91,24 @@ add_things_def <- function(x = 1) {
 }
 add_things_def()
 
+
+#...........................
+
+# Environments
+#...........................
+environment()
+simple_fun <- function(){
+  environment()
+}
+simple_fun()
+
+# functions create their own environments!
+
 #...........................
 
 # Scoping
 #...........................
-
 # where does R find things?
-
 add_things2 <- function(x) {
   x + 10 + y
 }
@@ -55,19 +121,8 @@ add_things2(2)
 
 #...........................
 
-# Lazy Evaluation
-#...........................
-add_things3 <- function(x, y) {
-  x + 10
-}
-add_things3(2)
-
-
-#...........................
-
 # If statements
 #...........................
-
 mean2 <- function(x, na.rm = FALSE) {
   if (na.rm){
     x <- x[!is.na(x)]
@@ -80,24 +135,26 @@ mean3 <- function(x, na.rm = FALSE) {
   if(na.rm){
     x <- x[!is.na(x)]
   } 
-  if(length(x) == 1) stop("'x' is too short to calculate a mean!")
+  if(length(x) == 0) stop("'x' is way too short to calculate a mean!")
+  if(length(x) == 1) warning("'x' is too short to calculate a mean!")
   sum(x)/length(x)
 }
 
+mean3(numeric())
 mean3(2)
 
 
-# Errors, warnings etc.
+# Early returns
 mean4 <- function(x, na.rm = FALSE) {
   if(na.rm){
     x <- x[!is.na(x)]
   } 
-  if(length(x) == 1) stop("'x' is too short to calculate a mean!")
-  if(length(x) == 0) return(0)
+  if(length(x) == 0) return(NA)
   sum(x)/length(x)
 }
 
-mean4(2)
+mean4(numeric())
+mean(numeric())
 
 ## Alternatives
 ?stopifnot
@@ -107,11 +164,11 @@ mean4(2)
 
 # Writing your own function
 #...........................
-## Before
+### Before
 # What should my function do?
 # Input, output
 
-## Process
+### Process
 # Write function
 # Test it
 # Add input validation
--- syntaxes/05_functions_II.R
+++ syntaxes/05_functions_II.R
@@ -9,6 +9,75 @@
 #===========================================================
 #...........................
 
+# What is a good function?
+#...........................
+# pure functions!
+
+# no sideeffects
+
+# no dependency on global environment
+
+# easier understanding, easier transfer!
+
+#...........................
+
+# debugging
+#...........................
+# browser()
+pretty_table_dev <- function(x, x_label, useNA = "ifany"){
+  browser()
+  stopifnot(length(x) > 1)
+  tab <- table(x, useNA = useNA)
+  out <- as.data.frame(tab)
+  names(out) <- c(x_label, "Frequency")
+  out
+}
+pretty_table_dev(mtcars$cyl, x_label = "Cyl")
+
+ls()
+
+
+pretty_table <- function(x, x_label, useNA = "ifany"){
+  stopifnot(length(x) > 1)
+  tab <- table(x, useNA = useNA)
+  out <- as.data.frame(tab)
+  names(out) <- c(x_label, "Frequency")
+  out
+}
+pretty_table(mtcars$cyl, x_label = "Cyl")
+
+pretty_statistics <- function(x, x_label) {
+  m <- mean(x, na.rm = TRUE)
+  t <- pretty_table(x, x_label = x_label)
+  list(mean = m, table = t)
+}
+
+pretty_statistics(mtcars$cyl, x_label = "Cyl")
+
+by(mtcars, mtcars$carb, function(sub_dat) {
+  pretty_statistics(sub_dat$cyl, x_label = "Cyl")
+})
+
+# traceback
+traceback()
+
+
+# option(error = recover) and option(error = NULL)
+# option(warn = 2) 
+
+
+#...........................
+
+# Lazy Evaluation
+#...........................
+add_things3 <- function(x, y) {
+  x + 10
+}
+add_things3(2)
+
+
+#...........................
+
 # ... (dot dot dot)
 #...........................
 add_all_things <- function(...) {
@@ -25,36 +94,6 @@ add_all_things2(2, 3, 5, 10)
 
 #...........................
 
-# Environments
-#...........................
-
-
-#...........................
-
-# Pure functions
-#...........................
-# no sideeffects
-
-# no dependency on global environment
-
-
-#...........................
-
-# Anonymous functions
-#...........................
-# use case: functionals
-
-
-#...........................
-
-# Packages, Namespaces, ...
-#...........................
-haven::read_spss
-haven:::add_text
-
-
-#...........................
-
 # on.exit
 #...........................
 add_things <- function(x, y) {
@@ -64,6 +103,7 @@ add_things <- function(x, y) {
 }
 out <- add_things(1, 2)
 out
+
 
 
 # Exercises
