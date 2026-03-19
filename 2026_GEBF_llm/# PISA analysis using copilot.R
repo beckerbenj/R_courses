32f3d6dca2abf999b6552a6c0ae2b7222d28b1e9
Dries Debeer <debeer.dries@gmail.com>
2026-03-19 08:04:03 +0100

    morning edits

--- 2026_GEBF_llm/# PISA analysis using copilot.R
+++ 2026_GEBF_llm/# PISA analysis using copilot.R
@@ -1,8 +1,11 @@
-# PISA analysis using copilot
+# PISA analysis using copilot autocomplete
 
 # load PISA data
 data <- readRDS("data/pisaPlus_CF.RDS")
 
-# using the tidyverse compute the test restest reliability for the attitude scores (attitud_a - attitud_h)
-# first compute the average attitude score for t1 and t2, then compute the correlation between them
-# but first recode the attitude scores from -2 (strongly disagree) to 2 (strongly agree)
+# using the tidyverse compute the test restest reliability
+# for the attitude scores (attitud_a - attitud_h)
+# first compute the average attitude score for t1 and t2,
+# then compute the correlation between them
+# but first recode the attitude scores from -2 (strongly disagree)
+# to 2 (strongly agree)
